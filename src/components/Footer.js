529c75af3d6ad4c22426f4397e52fdcf7979b853
TANNISHTHA SENGUPTA <tannishthasengupta1@gmail.com>
2022-05-26 02:14:06 +0530

    initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class Footer extends Component {
+  render() {
+    return (
+<footer className="bg-light text-center text-lg-start">
+  <div className="text-center text-white p-1" style={{backgroundColor: '#4a6d8b'}}>
+  &copy; 2022 All Rights Reserved Newsvio
+  </div>
+</footer>
+    )
+  }
+}
+export default Footer
